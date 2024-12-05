e1cd8c0cb9edada3bdc4dfb518cf1adea1b00e9d
Papierkorb2292 <104673791+Papierkorb2292@users.noreply.github.com>
2024-12-05 14:51:29 +0100

    Updated to 1.21.4 (only yarn mapping changes)

--- src/main/java/net/papierkorb2292/multiscoreboard/SetUseMultiScoreboardS2CPacket.java
+++ src/main/java/net/papierkorb2292/multiscoreboard/SetUseMultiScoreboardS2CPacket.java
@@ -8,7 +8,7 @@ import net.minecraft.util.Identifier;
 
 public record SetUseMultiScoreboardS2CPacket(boolean useMultiScoreboard) implements CustomPayload {
     public static final Id<SetUseMultiScoreboardS2CPacket> ID = new CustomPayload.Id<>(Identifier.of(MultiScoreboard.MOD_ID, "use_multi_scoreboard"));
-    public static final Type<? super RegistryByteBuf, SetUseMultiScoreboardS2CPacket> TYPE = PayloadTypeRegistry.playS2C().register(ID, PacketCodecs.BOOL.xmap(SetUseMultiScoreboardS2CPacket::new, SetUseMultiScoreboardS2CPacket::useMultiScoreboard));
+    public static final Type<? super RegistryByteBuf, SetUseMultiScoreboardS2CPacket> TYPE = PayloadTypeRegistry.playS2C().register(ID, PacketCodecs.BOOLEAN.xmap(SetUseMultiScoreboardS2CPacket::new, SetUseMultiScoreboardS2CPacket::useMultiScoreboard));
 
     @Override
     public Id<SetUseMultiScoreboardS2CPacket> getId() {
